6d4d372215e6c7fae5017d33e3303adc8489673a
Malte Bublitz <malte70@mcbx.de>
2015-05-18 11:44:59 +0200

    First working version

---
+++ imageAutoRotate.php
@@ -0,0 +1,56 @@
+<?php
+
+
+function imageAutoRotate($originalFilename) {
+	if (mime_content_type($originalFilename) != "image/jpeg")
+		return False;
+	$exif = exif_read_data($originalFilename);
+	if(!$exif || !isset($exif['Orientation'])) {
+		return false;
+	} else {
+		$orientation = $exif['Orientation'];
+		$img = imagecreatefromjpeg($originalFilename);
+		$mirror = false;
+		$deg    = 0;
+
+		switch ($orientation) {
+			case 2:
+				$mirror = true;
+				break;
+			case 3:
+				$deg = 180;
+				break;
+			case 4:
+				$deg = 180;
+				$mirror = true;  
+				break;
+			case 5:
+				$deg = 270;
+				$mirror = true; 
+				break;
+			case 6:
+				$deg = 270;
+				break;
+			case 7:
+				$deg = 90;
+				$mirror = true; 
+				break;
+			case 8:
+				$deg = 90;
+				break;
+		}
+		if ($deg)
+			$img = imagerotate($img, $deg, 0); 
+		if ($mirror)
+			$img = imageflip($img, IMG_FLIP_BOTH);
+		return imagejpeg($img);
+	}
+}
+
+if (PHP_SAPI == 'cli') {
+	if (count($argv)!=2)
+		die("Usage: php imageAutoRotate.php <image>\n");
+
+	echo imageAutoRotate($argv[1]);
+}
+?>
